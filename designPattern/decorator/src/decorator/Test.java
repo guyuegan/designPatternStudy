7dd58963450d37b69246c833cce9bb7ad1a8a4dd
guyuegan <17789861157@163.com>
2018-12-18 10:13:07 +0800

    添加.gitignore文件

---
+++ designPattern/decorator/src/decorator/Test.java
@@ -0,0 +1,15 @@
+/**
+ * 装饰模式和代理模式不同点：
+ *   装饰模式客户端使用时，需要客户端提供要装饰的源对象
+ *   代理模式客户端使用时，客户端不用关心被代理的源对象（一般代理对象会组合一个源对象）
+ */
+public class Test {
+    public static void main(String[] args) {
+        Source source = new Source();
+        Decorator decorator = new Decorator(source);
+        System.out.println("=======================原始");
+        source.method();
+        System.out.println("=======================装饰");
+        decorator.method();
+    }
+}
